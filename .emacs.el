9eef5835adacf26a0c035ea97e3181a1309c1cc2
muramatsu <muramatsu@BSCMAC09.local>
2017-11-02 16:44:31 +0900

    add emacs.el .tmux.conf .zshrc

---
+++ .emacs.el
@@ -0,0 +1,127 @@
+;; ロードパスの設定
+;; (setq load-path (append
+;;                  '("~/.emacs.d")))
+
+;; 環境を日本語、UTF-8にする
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; 背景色を調整
+(set-face-background 'default "#000000")
+(set-face-foreground 'default "#f5f5f5")
+
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message t)
+
+;; バックアップファイルを作成させない
+(setq make-backup-files nil)
+
+;; 終了時にオートセーブファイルを削除する
+(setq delete-auto-save-files t)
+
+;; タブにスペースを使用する
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; 改行コードを表示する
+(setq eol-mnemonic-dos "(CRLF)")
+(setq eol-mnemonic-mac "(CR)")
+(setq eol-mnemonic-unix "(LF)")
+
+;; C-hをBackSpaceにする
+(keyboard-translate ?\C-h ?\C-?)
+
+;; 複数ウィンドウを禁止する
+(setq ns-pop-up-frames nil)
+
+;; ウィンドウを透明にする
+;; アクティブウィンドウ／非アクティブウィンドウ（alphaの値で透明度を指定）
+(add-to-list 'default-frame-alist '(alpha . (0.90 0.90)))
+
+;; メニューバーを消す
+(menu-bar-mode -1)
+
+;; ツールバーを消す
+(tool-bar-mode -1)
+
+;; 列数を表示する
+(column-number-mode t)
+
+;; 行数を表示する
+(global-linum-mode t)
+
+;; カーソルの点滅をやめる
+(blink-cursor-mode 0)
+
+;; カーソル行をハイライトする
+(global-hl-line-mode t)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+
+;; ウィンドウ内に収まらないときだけ、カッコ内も光らせる
+(setq show-paren-style 'mixed)
+(set-face-background 'show-paren-match-face "grey")
+(set-face-foreground 'show-paren-match-face "black")
+
+;; スペース、タブなどを可視化する
+;; (global-whitespace-mode 1)
+
+;; スクロールは１行ごとに
+(setq scroll-conservatively 1)
+
+;; シフト＋矢印で範囲選択
+;; (setq pc-select-selection-keys-only t)
+;; (pc-selection-mode t)
+
+;; exec-pathを実行
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+;; C-kで行全体を削除する
+(setq kill-whole-line t)
+
+;;; dired設定
+(require 'dired-x)
+
+;; "yes or no" の選択を "y or n" にする
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; beep音を消す
+(defun my-bell-function ()
+  (unless (memq this-command
+        '(isearch-abort abort-recursive-edit exit-minibuffer
+              keyboard-quit mwheel-scroll down up next-line previous-line
+              backward-char forward-char))
+    (ding)))
+(setq ring-bell-function 'my-bell-function)
+
+;; Macのoptionをメタキーにする
+(setq mac-option-modifier 'meta)
+
+;; markdown-mode
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+;; python-mode
+;; (add-hook 'python-mode-hook 'jedi:setup)
